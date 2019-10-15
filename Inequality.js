cc41c268c8e0e591bb977562dec64fe336371aeb
Shital <shital31>
2019-10-16 00:27:35 +0530

    practice examples

---
+++ Inequality.js
@@ -0,0 +1,7 @@
+function notequal(num){
+    if(num !==0){
+        return "Not Equal";
+    }
+    return "Equal";
+}
+console.log(notequal(0));
---
+++ Logicaloperator.js
@@ -0,0 +1,7 @@
+function testlogicalAnd(val){
+    if(val<=50 && val>=20){
+        return "yes";
+    }
+     return "no";
+}
+console.log(testlogicalAnd(10));
