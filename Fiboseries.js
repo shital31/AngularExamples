78356bc8bff277e399e0d980c8dac9a11ec973db
Shital <shital31>
2019-10-15 15:48:06 +0530

    Homework

--- Fiboseries.js
+++ Fiboseries.js
@@ -1,1 +1,13 @@
-f
+var fib = [0, 1];
+for(var i=fib.length; i<10; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+}
+console.log(fib); 
+
+/*function fibo(num){
+    for(var i=fibo.length;i<num;i++){
+        fibo=fibo[i-2]+fibo[i-1]
+    }
+    console.log(fibo)
+}
+fibo(5)*/
