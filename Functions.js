2b9a2cf18b046cbb69902577766737c41d76a59c
Shital <shital31>
2019-10-15 23:14:15 +0530

    practice examples

--- Functions.js
+++ Functions.js
@@ -2,3 +2,9 @@ function fn(){
     console.log("Hi World")
 }
 fn();
+
+//return value
+function sub(num){
+    return num-2;
+}
+console.log(sub(10));
---
+++ Ifelse.js
@@ -0,0 +1,15 @@
+function TrueorFalse(isItTrue){
+    if(isItTrue){
+        return "It is true"
+    }
+    return "It is false"
+}
+console.log(TrueorFalse(false));
+
+function trueorfalse(num){
+    if(num){
+        return "it is num";
+    }
+    return "its not a num";
+}
+console.log(trueorfalse());
