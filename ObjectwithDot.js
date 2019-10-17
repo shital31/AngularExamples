dddbf94515a7959480f4148d9bdfc25ca6a63335
Shital <shital31>
2019-10-18 00:59:21 +0530

    practice example

---
+++ ObjectwithDot.js
@@ -0,0 +1,10 @@
+var testObj = {
+    "hat": "Nike",
+    "Jeans":"Puma",
+    "Shirt":"Adidas",
+    "Shoes":"Nike"
+};
+var hatValue = testObj.hat;  ////find the value of that object using dot
+var ShoesValue = testObj.Shoes;
+console.log(hatValue);
+console.log(ShoesValue);
