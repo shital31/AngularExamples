e7a769f39b742f23bdfa9f0d44e65859c7b81bc5
Shital <shital31>
2019-10-18 14:55:15 +0530

    practice example

---
+++ ObjectsArray.js
@@ -0,0 +1,11 @@
+var plants = [{
+    type: "flowers",
+    list:["rose","tulip","jasmine"]
+},
+{type : "trees",
+list : ["fir","pine","birch"]
+}
+    
+];
+var tree = plants[1].list[2];
+console.log(tree);
