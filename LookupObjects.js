3e020ace2822dd806ac103539627fe655ae0df73
Shital <shital31>
2019-10-18 13:29:47 +0530

    practice example

--- LookupObjects.js
+++ LookupObjects.js
@@ -23,4 +23,22 @@
 
 
  //complex objects
+ var myMusic = [
+     {
+         "artist":"Arman Malik",
+         "title": "Pioan Man",
+         "release_year":"2019",
+         "Formats":["CD","8T","LP"],
+         "gold":true
+     },
+     {
 
+        "artist":"Carnes",
+        "title":"Man",
+        "Release_year": "2018",
+        "Formats":["CD","MP3","Audio"],
+        "NewRelease":true
+
+     }
+ ]
+ console.log(myMusic);
