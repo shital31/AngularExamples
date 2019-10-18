9ce46b9e936af06ef5b968091499295a99b67c6c
Shital <shital31>
2019-10-18 23:49:55 +0530

    practice examples

---
+++ Iterableloop.js
@@ -0,0 +1,14 @@
+var Array =[];
+var i =0
+while(i<10){
+    Array.push(i);
+    i++
+}
+console.log(Array);
+
+//using push
+var ourArray = [];
+for(var i=10;i>0;i -=2 ){
+    ourArray.push(i);
+}
+console.log(ourArray);
