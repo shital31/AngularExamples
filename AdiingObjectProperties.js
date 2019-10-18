8a5b18d6a21e8ea634376d343678d22da97893be
Shital <shital31>
2019-10-18 12:42:53 +0530

    prctice example

---
+++ AdiingObjectProperties.js
@@ -0,0 +1,10 @@
+var Dog = {
+    "name" : "Browny",
+    "Color": "Brown",
+    "Tail": 1,
+    "Friends":["Rockey","Tommy","Blu","Pasta"]
+
+};
+Dog.name = "Coco";
+Dog.bark= "Bow-bow";
+console.log(Dog);
