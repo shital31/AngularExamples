9d14e32eed019ac020237256267ac6c3480316d7
Shital <shital31>
2019-10-18 12:17:30 +0530

    practice examples

---
+++ ObjectwithBracket.js
@@ -0,0 +1,8 @@
+var testObj = {
+    "an tree" : "Apple",
+    "my side" : "Red",
+
+};
+
+var treeValue = testObj["an tree"]; //we can use bracket but it must have space in it 
+console.log(treeValue);
